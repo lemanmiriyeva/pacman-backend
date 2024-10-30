27b84140a18ef60b9b88d07f134b4aae0979d9b9
Ləman Bəşirova <laman.bashirova@mdm.local>
2024-10-30 11:57:55 +0400

    initial commit

--- pacmanBackend/settings.py
+++ pacmanBackend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9!ueq$2pn$*w1$p=hr2_r!5o9nfl9tc=266dzq339it1k^%z%-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
