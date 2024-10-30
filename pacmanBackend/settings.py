bf23e733306fb4c337aba765b97722e1fd42a337
Ləman Bəşirova <laman.bashirova@mdm.local>
2024-10-30 12:00:16 +0400

    initial commit

--- pacmanBackend/settings.py
+++ pacmanBackend/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
