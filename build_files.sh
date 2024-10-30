dfd88b9de76b4e876041e7f066afdb00f3215b94
NihadMemmedli <nihad.mammadli95@gmail.com>
2024-10-30 14:47:06 +0400

    fix

--- build_files.sh
+++ build_files.sh
@@ -13,4 +13,6 @@ python3.9 manage.py collectstatic --noinput
 # collect static files using the Python interpreter from venv
 python3.9 manage.py migrate --noinput
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
+
 echo "BUILD END"
